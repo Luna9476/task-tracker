1dd1ab6dbd0468412c6ad53dd5f2475089d9a48b
Luna <luleinju9476@gmail.com>
2021-12-25 15:06:40 -0800

    Basic implementation

---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Button = ({ text, color, onClick }) => {
+    return (
+        <button
+            onClick={onClick}
+            className='btn'
+            style={{ backgroundColor: color }}
+        >
+            {text}
+        </button>
+    )
+}
+
+export default Button
